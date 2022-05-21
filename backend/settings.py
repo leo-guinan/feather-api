f4e33257b0a185a9c19f9deffb353c1a132dfd78
leo-guinan <leo@copy.ai>
2022-05-21 13:56:31 -0400

    forgot to remove middleware

--- backend/settings.py
+++ backend/settings.py
@@ -56,7 +56,6 @@ SITE_ID = 2
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
