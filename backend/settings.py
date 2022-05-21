9f937c41f84d64baa1242cc05fee399dbb4c3af3
leo-guinan <leo@copy.ai>
2022-05-21 14:19:38 -0400

    adding static files app

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
