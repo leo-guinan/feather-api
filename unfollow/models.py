cd02cda68600e5fa18fe0a2decf6ffd2e3f0b646
leo-guinan <leo@copy.ai>
2022-06-12 15:00:50 -0400

    Removing unique check on model save because didn't work

--- unfollow/models.py
+++ unfollow/models.py
@@ -23,16 +23,6 @@ class Group(models.Model):
     name = models.CharField("Group Name", max_length=255, null=False)
     members = models.ManyToManyField("unfollow.TwitterAccount", related_name="member_of")
 
-    def validate_unique(self, exclude=None):
-        groups = Group.objects.filter(name=self.name)
-        if groups.filter(owned_by=self.owned_by).exists():
-            raise ValidationError('Name must be unique per site')
-
-    def save(self, *args, **kwargs):
-        self.validate_unique()
-
-        super(Group, self).save(*args, **kwargs)
-
     def __str__(self):
         return f'{self.owned_by}: {self.name}'
 
