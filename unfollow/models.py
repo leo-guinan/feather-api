b7273e38e39a4136f5dcae53ce456d9da8d0dea4
leo-guinan <leo@copy.ai>
2022-06-12 14:44:33 -0400

    Removing unique constraint and seeing if a creation of a group will work in prod

--- unfollow/models.py
+++ unfollow/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -22,13 +23,19 @@ class Group(models.Model):
     name = models.CharField("Group Name", max_length=255, null=False)
     members = models.ManyToManyField("unfollow.TwitterAccount", related_name="member_of")
 
+    def validate_unique(self, exclude=None):
+        groups = Group.objects.filter(name=self.name)
+        if groups.filter(owned_by=self.owned_by).exists():
+            raise ValidationError('Name must be unique per site')
+
+    def save(self, *args, **kwargs):
+        self.validate_unique()
+
+        super(Group, self).save(*args, **kwargs)
+
     def __str__(self):
         return f'{self.owned_by}: {self.name}'
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['name', 'owned_by'], name='unique_user_group_name'),
-        ]
 
 
 
