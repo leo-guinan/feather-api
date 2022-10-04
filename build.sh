e0cf698825808b566f71184c704ba8b5fc8d57c7
leo-guinan <leo@copy.ai>
2022-10-03 20:11:14 -0400

    fixing build script because render

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 poetry run pip install --upgrade pip
-poetry run pip install --upgrade setuptools
+poetry run pip install --force-reinstall -U setuptools
 poetry install
 
 python manage.py collectstatic --no-input
