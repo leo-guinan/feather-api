1d9a1757c1d0bf2cef22aac580e45c360e557dcd
leo-guinan <leo@copy.ai>
2022-10-03 20:23:50 -0400

    fixing build script to remove poetry use

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-poetry run pip install --upgrade pip
-poetry run pip install --force-reinstall -U setuptools
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 poetry install
 
 python manage.py collectstatic --no-input
