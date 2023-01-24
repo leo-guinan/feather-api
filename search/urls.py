1925a5f4ec28a8ac56a0cba61eee9d2030b8c204
leo-guinan <leo@copy.ai>
2023-01-23 19:49:27 -0500

    adding endpoint

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from search.views import search_creator_content
+from search.views import search_creator_content, search_curated_content
 
 urlpatterns = [
     path('search/', search_creator_content),
+    path('curated/', search_curated_content),
 
 ]
