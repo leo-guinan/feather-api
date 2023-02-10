d1d5d1e893320110511c80b600a47263e3a71622
leo-guinan <leo@copy.ai>
2023-02-10 17:53:06 -0500

    removing files when done so I don't run out of space

--- whisper/whisper.py
+++ whisper/whisper.py
@@ -4,20 +4,19 @@ import logging
 import requests
 from decouple import config
 
+import os
 from os.path import splitext
 from pydub import AudioSegment
 
 
 
-def wav_to_flac(wav_path):
-    flac_path = "%s.flac" % splitext(wav_path)[0]
+def wav_to_flac(wav_path, flac_path):
     song = AudioSegment.from_wav(wav_path)
     song.export(flac_path, format="flac")
     return flac_path
 
 
-def mp3_to_flac(mp3_path):
-    flac_path = "%s.flac" % splitext(mp3_path)[0]
+def mp3_to_flac(mp3_path, flac_path):
     song = AudioSegment.from_mp3(mp3_path)
     song.export(flac_path, format="flac")
     return flac_path
@@ -37,23 +36,25 @@ class Whisper:
         transcript.status = Transcript.TranscriptStatus.PROCESSING
         transcript.save()
         url = transcript.episode.download_link
+        file_name = url.split("/")[-1]
+        file_name = file_name.split("?")[0]
+        converted_filename = "%s.flac" % splitext(file_name)[0]
         try:
 
             logger.info("Downloading podcast from %s", url)
             # download the file
             r = requests.get(url)
             # get the file name
-            file_name = url.split("/")[-1]
-            file_name = file_name.split("?")[0]
+
             with open(file_name, "wb") as f:
                 f.write(r.content)
             # convert to flac
             converted_file = ""
             logger.info("Converting podcast to flac")
             if file_name.endswith(".mp3"):
-                converted_file = mp3_to_flac(file_name)
+                converted_file = mp3_to_flac(file_name, converted_filename)
             elif file_name.endswith(".wav"):
-                converted_file = wav_to_flac(file_name)
+                converted_file = wav_to_flac(file_name, converted_filename)
 
             files = [('files', open(converted_file, 'rb'))]
             logger.info("Sending podcast to endpoint")
@@ -74,4 +75,8 @@ class Whisper:
             transcript.error = str(e)
             transcript.save()
             raise e
+        finally:
+            os.remove(file_name)
+            os.remove(converted_filename)
+
 
